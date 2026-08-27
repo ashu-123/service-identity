83167a2f049629c6345dee3d309b5c480a4f3b06
Ashutosh <Ashutosh>
2026-08-27 13:33:52 +0530

    configuring duration for email verification

---
+++ src/main/java/com/common/identity/auth/config/EmailVerificationProperties.java
@@ -0,0 +1,18 @@
+package com.common.identity.auth.config;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.Duration;
+
+@Getter
+@Setter
+@Configuration
+@ConfigurationProperties(prefix = "app.email-verification")
+public class EmailVerificationProperties{
+
+    private Duration expiration;
+
+}
